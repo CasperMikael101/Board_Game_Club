fbd20bba54991f9f01bd59c980f1910f070e6d1f
Akam <157468108+AkamNabi@users.noreply.github.com>
2024-11-14 12:58:22 +0200

    lol

--- board_game_club/apps.py
+++ board_game_club/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class BoardGameClubConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'board_game_club'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "board_game_club"
